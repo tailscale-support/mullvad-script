5147dd8d2132e3110dba904abe6b86dedf20b153
Tim Walters <tim@tailscale.com>
2024-07-14 22:15:24 -0400

    update the script to use sh
    Allows for wider usage of the script by updating to sh for POSIX
    compatibility.
    
    Fixes #1

--- signNodes.sh
+++ signNodes.sh
@@ -1,32 +1,38 @@
-#! /bin/zsh
+#! /bin/sh
 
-#prompt the user to confirm that they want to sign all nodes
-echo 'Do you want to sign all Mullvad nodes?'
-if read -q "choice0?Y/y to confirm"; then
-  #ask which country nodes to sign
-  echo '\nInput the two letter country code for the Mullvad exit nodes you would like to sign.\nUse ".." to sign all nodes.'
-  read country
-  if [[ ${#country} -gt 2 ]]; then
-    echo "Country code longer than 2 characters, please only enter 2 characters."
-    return
+# Store the command in a variable
+tsBinary=$(which tailscale)
+
+# Prompt the user to confirm that they want to sign all nodes
+printf "Do you want to sign all Mullvad nodes?\n"
+printf "Y/y to confirm: "
+read -r choice0
+printf "\n" # To move to a new line after input
+
+if [ "$choice0" = "Y" ] || [ "$choice0" = "y" ]; then
+  # Ask which country nodes to sign
+  printf '\nInput the two letter country code for the Mullvad exit nodes you would like to sign.\nUse ".." to sign all nodes.\n'
+  read -r country
+  if [ ${#country} -gt 2 ]; then
+    printf "Country code longer than 2 characters, please only enter 2 characters.\n"
+    exit 1
   fi
-  #store all unsigned nodes
-  lockedNodes=($(/Applications/Tailscale.app/Contents/MacOS/Tailscale lock status --json | jq -rc '.FilteredPeers[]'))
-  #loop thorugh all locked nodes
-  for node in $lockedNodes; do
-    if [[ "$(echo $node | jq -r .Name)" =~ "^$country.*mullvad\.ts\.net\.$" ]]; then
-      echo "Signing node $(echo $node | jq .Name)"
-      /Applications/Tailscale.app/Contents/MacOS/Tailscale lock sign $(echo $node | jq -r '.NodeKey')
+  # Store all unsigned nodes
+  lockedNodes=$($tsBinary lock status --json | jq -rc '.FilteredPeers[]')
+  # Loop through all locked nodes
+  echo "$lockedNodes" | while read -r node; do
+    nodeName=$(echo "$node" | jq -r .Name)
+    if echo "$nodeName" | grep -q "^$country.*mullvad\.ts\.net\.$"; then
+      printf "Signing node %s\n" "$nodeName"
+      $tsBinary lock sign $(echo "$node" | jq -r '.NodeKey')
     else
-      #if verbose logging is enabled, show skipped nodes
-      if [[ $1 == "-v" ]]; then
-        echo "Node $(echo $node | jq .Name) skipped"
+      # If verbose logging is enabled, show skipped nodes
+      if [ "$1" = "-v" ]; then
+        printf "Node %s skipped\n" "$nodeName"
       fi
     fi
   done
-  
 else
-	echo ""
-	echo "'$choice0' not 'Y' or 'y'. Exiting..."
-	return 1
+  printf "\n'%s' not 'Y' or 'y'. Exiting...\n" "$choice0"
+  exit 1
 fi
