02472ba219b5d63a44793583cd613d69fb5abe9e
Garrett Orsech <110842475+garrett-ts@users.noreply.github.com>
2024-11-20 15:18:05 -0500

    add check for sudo and doas (#5)

--- signNodes.sh
+++ signNodes.sh
@@ -3,6 +3,26 @@
 # Store the command in a variable
 tsBinary=$(which tailscale)
 
+# Check if root and whether to use sudo or doas
+	CAN_ROOT=
+	SUDO=
+	if [ "$(id -u)" = 0 ]; then
+		CAN_ROOT=1
+		SUDO=""
+	elif type sudo >/dev/null; then
+		CAN_ROOT=1
+		SUDO="sudo"
+	elif type doas >/dev/null; then
+		CAN_ROOT=1
+		SUDO="doas"
+	fi
+	if [ "$CAN_ROOT" != "1" ]; then
+		echo "This installer needs to run commands as root."
+		echo "We tried looking for 'sudo' and 'doas', but couldn't find them."
+		echo "Either re-run this script as root, or set up sudo/doas."
+		exit 1
+	fi
+
 # Prompt the user to confirm that they want to sign all nodes
 printf "Do you want to sign all Mullvad nodes?\n"
 printf "Y/y to confirm: "
@@ -24,7 +44,7 @@ if [ "$choice0" = "Y" ] || [ "$choice0" = "y" ]; then
     nodeName=$(echo "$node" | jq -r .Name)
     if echo "$nodeName" | grep -q "^$country.*mullvad\.ts\.net\.$"; then
       printf "Signing node %s\n" "$nodeName"
-      $tsBinary lock sign $(echo "$node" | jq -r '.NodeKey')
+      $SUDO "$tsBinary" lock sign "$(echo "$node" | jq -r '.NodeKey')"
     else
       # If verbose logging is enabled, show skipped nodes
       if [ "$1" = "-v" ]; then
